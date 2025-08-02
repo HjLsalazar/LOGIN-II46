d34527fbe06e2d3fe5c9cbdd0ac0b774a53d92cd
HjLsalazar <163606026+HjLsalazar@users.noreply.github.com>
2025-08-01 21:29:58 -0600

    Final de la clase

--- Home.aspx
+++ Home.aspx
@@ -1,3 +1,7 @@
 ﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Home.aspx.vb" Inherits="PROYECTO.Home" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Bienvenido</h1>
+    <asp:Label ID="lblNombre" runat="server" Text=""></asp:Label>
+    <asp:Label ID="lblEmail" runat="server" Text=""></asp:Label>
 </asp:Content>
+
--- Registro.aspx.vb
+++ Registro.aspx.vb
@@ -1,8 +1,58 @@
-﻿Public Class Registro
+﻿Imports System.Data.SqlClient
+
+Public Class Registro
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
 
+    Protected Sub btnRegistrar_Click(sender As Object, e As EventArgs) Handles btnRegistrar.Click
+        Dim nombre As String = txtNombre.Text
+        Dim apellido As String = txtApellido.Text
+        Dim email As String = txtEmail.Text
+        Dim pass As String = txtPass.Text
+
+        Dim usuario As New Usuario() With {
+            .Email = txtEmail.Text,
+            .Password = txtPass.Text,
+            .Nombre = txtNombre.Text,
+            .Apellido = txtApellido.Text
+        }
+        If RegistrarUsuario(usuario) Then
+            ' Redirigir al login o a la página de inicio
+
+            ScriptManager.RegisterStartupScript(
+                Me, Me.GetType(),
+                "ServerControlScript",
+                "Swal.fire('Usuario Registrado').then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = 'Login.aspx';
+                    }
+                });",
+                True)
+
+        Else
+            ScriptManager.RegisterStartupScript(
+                Me, Me.GetType(),
+                "ServerControlScript",
+                "Swal.fire('Error al registrar el usuario. Inténtalo de nuevo.');",
+                True)
+            lblError.Text = "Error al registrar el usuario. Inténtalo de nuevo."
+            lblError.Visible = True
+        End If
+
+    End Sub
+
+    Private Function RegistrarUsuario(usuario As Usuario) As Boolean
+        Dim helper As New DatabaseHelper()
+        Dim sql As String = "INSERT INTO Usuarios (Email, Contraseña, Nombre, Apellidos) VALUES (@Email, @Password, @Nombre, @Apellido)"
+        Dim parameters As New List(Of SqlParameter) From {
+            New SqlParameter("@Email", usuario.Email),
+            New SqlParameter("@Password", usuario.Password),
+            New SqlParameter("@Nombre", usuario.Nombre),
+            New SqlParameter("@Apellido", usuario.Apellido)
+        }
+        Return helper.ExecuteNonQuery(sql, parameters)
+    End Function
 End Class
